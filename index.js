4a843fd3b3aecd1a703ce84053a5f536849938b5
ashishpipaliya <ashish.pipaliya22@gmail.com>
2022-11-21 23:55:34 +0530

    airpay token

--- index.js
+++ index.js
@@ -74,8 +74,7 @@ app.post('/tokem', (req, res) => {
             })
 
             for(var i =0; i<arr.length; i++){
-                tokens+= `<p>${rr[i]}-${arr2[i]}</br></p>`
-                tokens+= "</br>"
+                tokens+= `<p>${arr[i]}-${arr2[i]}</p></br>`;
             }
             res.send(tokens);
         } catch (e) {
